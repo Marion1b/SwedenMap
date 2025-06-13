32f93f8586ac0cfea52e025937ec8d6eac5686d6
Marion1b <marion.barthoux@gmail.com>
2025-06-13 16:01:30 +0200

    create burger button

---
+++ src/components/BurgerButton.tsx
@@ -0,0 +1,13 @@
+import "../css/components/BurgerButton.css";
+
+const BurgerButton = () =>{
+    return(
+        <button className="burger-button">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+    )
+}
+
+export default BurgerButton;
