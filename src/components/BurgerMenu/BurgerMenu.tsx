72bee48a584cdd9d570bb28c2086a345335302df
Marion1b <marion.barthoux@gmail.com>
2025-06-15 12:47:42 +0200

    feat: burger menu style done

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -4,42 +4,70 @@ import { useState } from "react";
 const BurgerMenu = ({isOpen}:{isOpen:boolean}) =>{
     const [affichageOpen, setAffichageOpen]=useState<boolean>(false);
     const[categoriesOpen, setCategoriesOpen]=useState<boolean>(false);
+    const [affichageActive, setAffichageActive] = useState<boolean>(false);
 
     const handleClickAffichage = () =>{
         setAffichageOpen(!affichageOpen);
+        if(categoriesOpen){
+            setCategoriesOpen(false);
+        }
     }
 
     const handleClickCategories=()=>{
         setCategoriesOpen(!categoriesOpen);
+        if(affichageOpen){
+            setAffichageOpen(false);
+        }
+    }
+
+    const handleClickInputAffichage=()=>{
+        setAffichageActive(!affichageActive);
     }
 
     return(
-        <nav className={`burger-menu burger-menu-${isOpen}`}>
+        <nav className={`burger-menu burger-menu-open-${isOpen}`}>
             <ul>
                 <li><span className="triangle"></span>Mon compte</li>
-                <li onClick={handleClickAffichage}><span className="triangle"></span>Affichage</li>
-                    <form action="" className={`affichage-open-${affichageOpen}`}>
-                        <input type="radio" id="informations-parameters" name="informations-parameters"/>
-                        <label htmlFor="informations-parameters">Afficher toutes les informations des lieux</label>
-                    </form>
-                <li onClick={handleClickCategories}><span className="triangle"></span>Catégories</li>
-                    <form action="" className={`categories-open-${categoriesOpen}`}>
-                        <fieldset>
-                            <legend>Catégories affichées : </legend>
-                            <div>
-                                <input type="checkbox" name="categories" id="museum" />
-                                <label htmlFor="museum">musée</label>
-                                <input type="checkbox" name="categories" id="coffee" />
-                                <label htmlFor="coffee">café</label>
-                                <input type="checkbox" name="categories" id="park" />
-                                <label htmlFor="park">parc</label>
-                                <input type="checkbox" name="categories" id="restaurant" />
-                                <label htmlFor="restaurant">restaurant</label>
-                                <input type="checkbox" name="categories" id="shop" />
-                                <label htmlFor="shop">magasin</label>
-                            </div>
-                        </fieldset>
-                    </form>
+                <div>
+                    <li onClick={handleClickAffichage}><span className={`triangle triangle-open-${affichageOpen}`}></span>Affichage</li>
+                        <form action="" className={`affichage-open-${affichageOpen}`}>
+                            <label htmlFor="informations-parameters">Afficher toutes les informations des lieux</label>
+                            <input type="button" id="informations-parameters" name="informations-parameters" className={`affichage-active-${affichageActive}`} onClick={handleClickInputAffichage}/>
+                        </form>
+                </div>
+                <div>
+                    <li onClick={handleClickCategories}><span className={`triangle triangle-open-${categoriesOpen}`}></span>Catégories</li>
+                        <form action="" className={`categories-open-${categoriesOpen}`}>
+                            <fieldset>
+                                <legend>Catégories affichées : </legend>
+                                <div>
+                                    <input type="checkbox" name="categories" id="museum" />
+                                    <span className="checkmark"></span>
+                                    <label htmlFor="museum">musée</label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="categories" id="coffee" />
+                                    <span className="checkmark"></span>
+                                    <label htmlFor="coffee">café</label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="categories" id="park" />
+                                    <span className="checkmark"></span>
+                                    <label htmlFor="park">parc</label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="categories" id="restaurant" />
+                                    <span className="checkmark"></span>
+                                    <label htmlFor="restaurant">restaurant</label>
+                                </div>
+                                <div>
+                                    <input type="checkbox" name="categories" id="shop" />
+                                    <span className="checkmark"></span>
+                                    <label htmlFor="shop">magasin</label>
+                                </div>
+                            </fieldset>
+                        </form>
+                    </div>
             </ul>
             <p>Informations légales</p>
         </nav>
