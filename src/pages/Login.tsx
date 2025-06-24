200055e1a4b20ee0dc52eceb47d3a8ce8d32f39e
Marion1b <marion.barthoux@gmail.com>
2025-06-24 15:00:59 +0200

    feat: little style changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,19 +6,21 @@ const Login = () =>{
         <>
         <BurgerMenuContainer />
         <section className="login">
-            <h1>Connexion</h1>
-            <form action="/account" method="POST">
-                <div>
-                    <label htmlFor="email">Email :</label>
-                    <input type="email" name="email" id="email" />
-                </div>
-                <div>
-                    <label htmlFor="password">Mot de passe :</label>
-                    <input type="password" name="password" id="password" />
-                </div>
-                <button type="submit">Connexion</button>
-            </form>
-            <p>Pas de compte ? <a href="/register">Créer un compte</a></p>
+            <div className="login-container">
+                <h1>Connexion</h1>
+                <form action="/account" method="POST">
+                    <div>
+                        <label htmlFor="email">Email :</label>
+                        <input type="email" name="email" id="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Mot de passe :</label>
+                        <input type="password" name="password" id="password" />
+                    </div>
+                    <button type="submit">Connexion</button>
+                </form>
+                <p>Pas de compte ? <a href="/register">Créer un compte</a></p>
+            </div>
         </section>
         </>
     )
