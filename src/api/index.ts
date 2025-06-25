6e1be6e0f6d5824a3e801c75e1ba615e1c4126b0
Marion1b <marion.barthoux@gmail.com>
2025-06-25 12:02:49 +0200

    begin fetching api

---
+++ src/api/index.ts
@@ -0,0 +1,33 @@
+interface dataToSendRegister {
+    email: string,
+    pseudo: string,
+    password: string
+}
+
+export default class Routes{
+    private apiUrl:string|undefined = process.env.API_URL;
+
+    public async register(data:dataToSendRegister){
+        try{
+            if(!this.apiUrl){
+                return console.error('api url is undefined');
+            }
+            const response = await fetch(this.apiUrl,{
+                method:'POST',
+                headers:{
+                    'Content-type':'application/json'
+                },
+                body:JSON.stringify(data)
+            })
+
+            if(!response.ok){
+                throw new Error('Network response was not ok');
+            }
+
+            const apiResponse = await response.json();
+            return {apiResponse, error:null, status:'success'};
+        }catch(error){
+            return{apiResponse:null, error:error, status:'error'};
+        }
+    }
+}
