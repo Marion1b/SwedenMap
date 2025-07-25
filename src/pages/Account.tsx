bfc101466c52c137b750428e53915bbb77fbd7d0
Marion1b <marion.barthoux@gmail.com>
2025-07-25 15:28:38 +0200

    feat:style account page

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,9 +1,23 @@
+import BurgerMenuContainer from "../components/BurgerMenu/BurgerMenuContainer";
+import "../css/pages/Account.css";
+
 const Account = () => {
     return(
-        <section className="account">
-            <h1>Account</h1>
-            <a href="/login">login</a>
-        </section>
+        <>
+            <BurgerMenuContainer />
+            <section className="account">
+                <h1 hidden>Mon compte</h1>
+                <div className="account-container">
+                    <h2>{sessionStorage.getItem('username')}</h2>
+                    <ul>
+                        <li><i className="fi fi-br-plus"></i>Créer une nouvelle map</li>
+                        <li><i className="fi fi-br-map-point"></i>Consulter mes maps</li>
+                        <li><i className="fi fi-br-bookmark"></i>Maps sauvegardées</li>
+                        <li><i className="fi fi-br-gears"></i>Paramètres</li>
+                    </ul>
+                </div>
+            </section>
+        </>
     )
 }
 
