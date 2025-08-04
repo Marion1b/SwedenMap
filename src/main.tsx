9ad9c2e8797c6341605d42966008f5dc6844ce6f
Marion1b <121030423+Marion1b@users.noreply.github.com>
2025-08-04 11:41:14 +0200

    fix: app architecture fix (#25)

--- src/main.tsx
+++ src/main.tsx
@@ -1,63 +1,9 @@
 //React
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router'
-
-//style
-import './css/index.css'
-
-//protected route
-import ProtectedRoute from './components/ProtectedRoute.tsx'
 
 //pages
-import App from './pages/App.tsx'
-import Account from './pages/Account.tsx'
-import Login from './pages/Login.tsx'
-import Register from './pages/Register.tsx'
-import CreateMap from './pages/CreateMap.tsx'
-import Settings from './pages/Settings.tsx'
-
-const user = {
-  userId: sessionStorage.getItem('userId')||undefined,
-  accessToken: sessionStorage.getItem('accessToken')||undefined
-}
+import App from './pages/App'
 
 createRoot(document.getElementById('root')!).render(
-    <Router>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path="/login" element={<Login />}/>
-        <Route path="/register" element={<Register />} />
-
-        {/**Protected routes */}
-        <Route 
-          path="/account"
-          element={
-            <ProtectedRoute user={user}>
-              <Account />
-            </ProtectedRoute>
-
-          }
-        />
-
-        <Route 
-          path="/create-map"
-          element={
-            <ProtectedRoute user={user}>
-              <CreateMap />
-            </ProtectedRoute>
-
-          }
-        />
-
-        <Route 
-          path={`/settings`}
-          element={
-            <ProtectedRoute user={user}>
-              <Settings />
-            </ProtectedRoute>
-
-          }
-        />
-      </Routes>
-    </Router>
+    <App />
 )
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,140 +1,66 @@
+//React
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+
 //style
-import '../css/pages/App.css'
+import '../css/index.css'
 
-//related to leaflet
-import { MapContainer, TileLayer, useMapEvents} from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import { type LatLngTuple } from 'leaflet';
+//protected route
+import ProtectedRoute from '../components/ProtectedRoute.tsx'
 
-//react utils
-import { useEffect, useState } from 'react';
+//pages
+import Home from './Home.tsx'
+import Account from './Account.tsx'
+import Login from './Login.tsx'
+import Register from './Register.tsx'
+import CreateMap from './CreateMap.tsx'
+import Settings from './Settings.tsx'
 
-//utils
-import { fetchActivities } from '../utils/fetchActivities';
-
-//components
-import PinContainer from '../components/PinContainer';
-import UserPosition from '../components/UserPosition';
-import RoutingMachine from '../components/RoutingMachine';
-import BurgerMenuContainer from '../components/BurgerMenu/BurgerMenuContainer';
-
-interface Activity{
-    name:string;
-    description:string;
-    geoloc:LatLngTuple;
-    category:string;
+const user = {
+  userId: sessionStorage.getItem('userId')||undefined,
+  accessToken: sessionStorage.getItem('accessToken')||undefined
 }
 
-interface Activities{
-    [key:string]:Activity;
-}
+const App = () =>{
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />} />
 
+        {/**Protected routes */}
+        <Route 
+          path="/account"
+          element={
+            <ProtectedRoute user={user}>
+              <Account />
+            </ProtectedRoute>
 
-function App() {
-  const [pinPosition, setPinPosition] = useState<null|LatLngTuple>(null); //position clicked
-  const [activitiesPositions, setActivitiesPositions]=useState<LatLngTuple[]>([]); // get all geoloc from the API
-  const [closestActivity, setClosestActivity]=useState<null|LatLngTuple>(null); // get geoloc of the closest activity clicked
-
-  //assign all geoloc from the api to activitiesPosition
-  useEffect(():void=>{
-    const fetchAndSetActivities = async():Promise<void |LatLngTuple[]> => {
-      let allPositions: LatLngTuple[]=[];
-      fetchActivities()
-        .then((result:{})=>{
-          const activities:Activities = result;
-          Object.keys(activities).map((key)=>{
-            const activity:Activity = activities[key];
-            allPositions.push(activity.geoloc);
-            return allPositions;
-          }, []);
-          setActivitiesPositions(allPositions);
-        })
-        .catch((error)=>{
-          console.error(error);
-        })
-    }
-    fetchAndSetActivities();
-  }, []);
-  
-
-  // get clicked geoloc
-  const LocationFinderDummy = ():null =>{
-    useMapEvents({
-        click(e) {
-          setPinPosition([e.latlng.lat, e.latlng.lng]);
-        },
-    });
-    return null;
-  }
-
-  // associate clicked geoloc to the nearest activity geoloc
-  const getNearestActivity = (clickedPosition: null|LatLngTuple):LatLngTuple|string =>{
-    if(clickedPosition && activitiesPositions){
-      let nearPoint:number|null = null;
-      //get key in the activitiesPosition array
-      let nearLatLngKey:number = -1;
-      const diffLatLngClickedPosition:number = Math.abs(clickedPosition[0] - clickedPosition[1]);
-
-      for(let i = 0; i<activitiesPositions.length; i++){
-        // calculate lat - lng
-        const lat:number = activitiesPositions[i][0];
-        const lng:number = activitiesPositions[i][1];
-        const diffLatLngActivity:number = Math.abs(lat - lng);
-
-        // condition to always get the bigger at the start of the operation
-        if(nearPoint){
-          if(diffLatLngActivity < diffLatLngClickedPosition && diffLatLngClickedPosition - diffLatLngActivity < nearPoint){
-            nearPoint = diffLatLngClickedPosition - diffLatLngActivity;
-            nearLatLngKey=i
-          }else if(diffLatLngClickedPosition < diffLatLngActivity && diffLatLngActivity - diffLatLngClickedPosition < nearPoint){
-            nearPoint = diffLatLngActivity - diffLatLngClickedPosition; 
-            nearLatLngKey = i;
           }
-          else if(diffLatLngActivity === diffLatLngClickedPosition){
-            nearPoint = diffLatLngActivity;
-            nearLatLngKey;
+        />
+
+        <Route 
+          path="/create-map"
+          element={
+            <ProtectedRoute user={user}>
+              <CreateMap />
+            </ProtectedRoute>
+
           }
-        }else{
-          if(diffLatLngActivity < diffLatLngClickedPosition){
-            nearPoint = diffLatLngClickedPosition - diffLatLngActivity;
-          }else{
-            nearPoint = diffLatLngActivity - diffLatLngClickedPosition;
+        />
+
+        <Route 
+          path={`/settings`}
+          element={
+            <ProtectedRoute user={user}>
+              <Settings />
+            </ProtectedRoute>
+
           }
-        }
-      }
-
-      return activitiesPositions[nearLatLngKey];
-    }
-    return `error, no correct clicked position or activities position`;
-  }
-
-  // watch clicked position to calculate the nearest activity
-  useEffect(() => {
-    if (pinPosition) {
-      const nearestActivity = getNearestActivity(pinPosition);
-      if(nearestActivity && typeof nearestActivity === "object" && typeof nearestActivity[0] === 'number' && typeof nearestActivity[1] === 'number'){
-        setClosestActivity(nearestActivity);
-      }else{
-        console.error("error while getting the nearest activity");
-      }
-      
-    }
-  }, [pinPosition]);
-
-  return (
-    <MapContainer center={[48.117538802169086, -1.6750580634024201]} zoom={13} scrollWheelZoom={false}>
-      <LocationFinderDummy />
-      <BurgerMenuContainer />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {closestActivity && <RoutingMachine destinationLocation={closestActivity} />}
-      <PinContainer />
-      <UserPosition />
-    </MapContainer>
-
+        />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
